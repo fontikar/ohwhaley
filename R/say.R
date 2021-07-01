3dc17923a1e7adce873de2479e4bec669aa8a80e
Fonti Kar <f.kar@unsw.edu.au>
2021-07-01 15:50:08 +1000

    Added phrases and readme

--- R/say.R
+++ R/say.R
@@ -1,14 +1,14 @@
 #' Summon a whale for a customisable pick-me-up
 #'
-#' @param what Whatever phrase you want whale to echo
-#'
+#' @param what Whatever phrase you want whale to echo. If not supplied, a random phrase is chosen
+#' @usage say(what)
 #' @return Whale shaped message
 #' @export
 #'
 #' @examples
 #' say("You're whale-come")
 
-say <- function(what = "How are you? I'm whaley good"){
+say <- function(what){
   #Defining the whale ASCII
   whale = "\n            ------ \n           %s \n            ------ \n               \\\   \n                \\\  \n                 \\\
      .-'
@@ -23,6 +23,11 @@ say <- function(what = "How are you? I'm whaley good"){
     regexpr('%s', whale)[1] - 1
 
   what_pos_end <- what_pos_start + 3
+
+  #If what isn't supplied...
+  if(missing(what)){
+    what <- sample(phrases, 1)
+  }
 
   #Combining positions, message and whale together
   out <- paste0(substr(whale, 1, what_pos_start), #Top of speech bubble
---
+++ README.md
@@ -0,0 +1,25 @@
+
+# ohwhaley
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+The goal of ohwhaley is to ...
+
+## Installation
+
+You can install the released version of ohwhaley from [CRAN](https://CRAN.R-project.org) with:
+
+``` r
+install.packages("ohwhaley")
+```
+
+## Example
+
+This is a basic example which shows you how to solve a common problem:
+
+``` r
+library(ohwhaley)
+## basic example code
+```
+
